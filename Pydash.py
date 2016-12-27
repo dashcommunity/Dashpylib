84ffca6aa95ecc41eafa93d853266d2285847cd8
Nova Trauben <noah.trauben@gmail.com>
2016-12-27 00:01:56 -0500

    cleaned up, added more
    Got rid of all the defaults on the subprocess call, added the communicate function. This does it right, without the mess that would return before. Also, I made the command execute again if something went wrong and did not happen.

--- Pydash.py
+++ Pydash.py
@@ -1,17 +1,24 @@
 import os
 import subprocess
+from time import sleep
 #run this module while ./dashd is running
 class Pydash:
 
 
     def __init__(self, dir):
         #check if we are in right directory
-        if not os.path.isfile('./dashd'):
+        if not os.path.isfile('./dashd'): #I have a feeling i am not doing this right. Will fix
             return "error: dir varable is not pointing toward ./dash-cli "
         self.dir = dir
 
 
     def command(self, arg):
-
         splitArg = arg.split()
-        return subprocess.Popen(splitArg, bufsize=0, executable=None, stdin=None, stdout=None, stderr=None, preexec_fn=None, close_fds=False, shell=False, cwd=self.dir, env=None, universal_newlines=False, startupinfo=None, creationflags=0)
+        for x in range(10):
+            sleep(.1)
+            info = subprocess.Popen(splitArg, shell=False, cwd=self.dir, ).communicate()
+            if info == None:
+                pass
+            else:
+                return info
+        #add zip thing to return info as dict, not turple
