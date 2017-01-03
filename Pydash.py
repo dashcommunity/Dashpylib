0ef847b82ff476461c028a39d0dbe28c12f27153
Nova Trauben <noah.trauben@gmail.com>
2017-01-02 21:56:48 -0500

    returns json output!!!
    testing.py doesnt work 100% though.

--- Pydash.py
+++ Pydash.py
@@ -1,24 +1,28 @@
 import os
-import subprocess
+from subprocess import check_output
 from time import sleep
+from json import loads
+from shlex import split
 #run this module while ./dashd is running
 class Pydash:
 
 
     def __init__(self, dir):
-        #check if we are in right directory
-        if not os.path.isfile('./dashd'): #I have a feeling i am not doing this right. Will fix
-            return "error: dir varable is not pointing toward ./dash-cli "
+        #check if global var, dir has ./dash-cli in the directory dir is pointing at
+        assert (not os.path.isfile('./dash-cli')), "global var 'dir', is not pointing toward a directory with ./dash-cli in it. or is file is under a different name? "
         self.dir = dir
 
 
     def command(self, arg):
+        #splits args into individual strings to work with shell syntax, using shlex
         splitArg = arg.split()
-        for x in range(10):
+        x = True
+        while x:
             sleep(.1)
-            info = subprocess.Popen(splitArg, shell=False, cwd=self.dir, ).communicate()
-            if info == None:
+            turp = check_output(splitArg, shell=False, cwd=self.dir, )
+            if turp == None:
                 pass
             else:
-                return info
-        #add zip thing to return info as dict, not turple
+                x = False
+                new = turp.translate(None, "\n")
+                return loads(new)
--- testing.py
+++ testing.py
@@ -1,4 +1,14 @@
 from Pydash import Pydash
-a = Pydash(dir="/home/nova/Downloads/dash-0.12.1/bin")
+from time import sleep
 
-print a.command("./dash-cli help") #better? ha
+Pydash = Pydash(dir="/home/nova/Downloads/dash-0.12.1/bin")
+execute = Pydash.command("./dash-cli getinfo")["blocks"]
+b = raw_input("what block do you want to be notified at? \ndashd is at block number " + str(execute) + "\n")
+trigger = True
+#print int(execute) >= int(b)
+
+while trigger:
+    if int(execute) >= int(b):
+        print "at " + str(execute) + "# block and... by the way the mining difficulty is " + str(Pydash.command("./dash-cli getinfo")["difficulty"])
+        print "bye"
+        trigger = False
