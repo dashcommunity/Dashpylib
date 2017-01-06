b97d378278b8f0b332366869bdfa0f8e225586f2
Nova Trauben <noah.trauben@gmail.com>
2017-01-05 22:59:24 -0500

    for pypy

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ setup(
     name="Dashpylib",
     version="0.1",
     scripts=["checks.py"],
-    version = "0.1",
     author="Nova Trauben",
     author_email="noah.trauben@gmail.com",
     description="A dash-cli wrapper module for python",
     license="MIT",
     url="https://github.com/dashcommunity/Dashpylib",
-    
+    download_url = "https://github.com/dashcommunity/Dashpylib/tarball/0.1",
 
 )
