e2a2bdc9b8f1823fe7a13b65682f69a30d2b344b
Nova Trauben <noah.trauben@gmail.com>
2017-01-07 14:31:19 -0500

    fixed setup.py, even thought it doesnt work at all threw setup.py at the moment

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     name="Dashpylib",
     version="0.1",
-    scripts=["checks.py"],
+    #scripts=["checks.py"],
     author="Nova Trauben",
     author_email="noah.trauben@gmail.com",
     description="A dash-cli wrapper module for python",
